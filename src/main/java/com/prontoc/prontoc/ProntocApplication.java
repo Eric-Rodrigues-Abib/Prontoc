e64cb8d33bc086354fa55af2b335fd6d1b57bb20
Eric Abib <ehheric@gmail.com>
2023-11-13 23:39:55 -0300

    Initial commit

---
+++ src/main/java/com/prontoc/prontoc/ProntocApplication.java
@@ -0,0 +1,13 @@
+package com.prontoc.prontoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProntocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProntocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prontoc/prontoc/ProntocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prontoc.prontoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProntocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
