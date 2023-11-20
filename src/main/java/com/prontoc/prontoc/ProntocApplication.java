6d43855bd01b0acdfaeeb6444f18fa05b0a97b15
Eric Abib <ehheric@gmail.com>
2023-11-20 13:22:57 -0300

    MONGO PROPERTIES

--- src/main/java/com/prontoc/prontoc/ProntocApplication.java
+++ src/main/java/com/prontoc/prontoc/ProntocApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProntocApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ProntocApplication.class, args);
 	}
